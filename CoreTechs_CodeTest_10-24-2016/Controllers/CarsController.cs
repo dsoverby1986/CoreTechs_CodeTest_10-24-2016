ef2f8064a5fd6236c0f3d2bda1cd1e801367d86c
Shane Overby <dsoverby1986@gmail.com>
2016-10-25 22:09:08 -0400

    Exception handling & validation

--- CoreTechs_CodeTest_10-24-2016/Controllers/CarsController.cs
+++ CoreTechs_CodeTest_10-24-2016/Controllers/CarsController.cs
@@ -16,56 +16,118 @@ namespace CoreTechs_CodeTest_10_24_2016.Controllers
         private HCL2Entities db = new HCL2Entities();
 
         // GET: Cars
-        public ActionResult Index(CarsIndexViewModel model)
+        public ActionResult Index()
         {
-            IQueryable<Car> cars = db.Cars;
-            model = new CarsIndexViewModel()
+            try
             {
-                Makes = cars.Select(c => new SelectListItem() { Value = c.make, Text = c.make }).Distinct().ToList(),
-                Models = new List<SelectListItem>(),
-                GridModel = new CarGridViewModel()
+                IQueryable<Car> cars = db.Cars;
+                if(cars != null)
                 {
-                    Cars = PagedListHelpers.GetNextCars(null, null, null, 0, 10)
+                    CarsIndexViewModel model = new CarsIndexViewModel()
+                    {
+                        Makes = cars.Select(c => new SelectListItem() { Value = c.make, Text = c.make }).Distinct().ToList(),
+                        Models = new List<SelectListItem>(),
+                        GridModel = new CarGridViewModel()
+                        {
+                            Cars = PagedListHelpers.GetNextCars(null, null, null, 0, 10)
+                        }
+                    };
+                    return View(model);
                 }
-            };
-            return View(model);
+                else
+                    throw new Exception("The 'Car' entity result collection is null.", new NoNullAllowedException());
+            }
+            catch(Exception ex)
+            {
+                return View("Error", new HandleErrorInfo(ex, "Cars", "Index"));
+            }
         }
 
         public ActionResult GetNextCars(string make, string model, string term, int? page, int pageSize = 10)
         {
-            var pageIndex = page - 1 ?? 0;
-            CarGridViewModel returnModel = new CarGridViewModel()
+            try
             {
-                Cars = PagedListHelpers.GetNextCars(make, model, term, pageIndex, pageSize),
-                Make = make,
-                Model = model,
-                Term = term
-            };
-            return PartialView("_CarsGrid", returnModel);
+                var pageIndex = page - 1 ?? 0;
+                CarGridViewModel returnModel = new CarGridViewModel()
+                {
+                    Cars = PagedListHelpers.GetNextCars(make, model, term, pageIndex, pageSize),
+                    Make = make,
+                    Model = model,
+                    Term = term
+                };
+                if (returnModel != null)
+                    return PartialView("_CarsGrid", returnModel);
+                else
+                    throw new Exception("The 'CarGridViewModel' object containing the 'Car' entity result collection is null.", new NoNullAllowedException());
+            }
+            catch(Exception ex)
+            {
+                return View("Error", new HandleErrorInfo(ex, "Cars", "Index"));
+            }
         }
 
         public ActionResult GetMakes(string term)
         {
-            var makes = db.Cars.Where(c => c.make.Contains(term) || c.model_name.Contains(term) || c.model_year.Contains(term) || c.model_trim.Contains(term)).Select(c => c.make).Distinct().OrderBy(c => c).ToList();
-            List<SelectListItem> makeList = makes.Select(m => new SelectListItem() { Value = m, Text = m }).ToList();
-            return PartialView("_MakesDropDownList", makeList);
+            try
+            {
+                var makes = db.Cars.Where(c => c.make.Contains(term) || c.model_name.Contains(term) || c.model_year.Contains(term) || c.model_trim.Contains(term)).Select(c => c.make).Distinct().OrderBy(c => c).ToList();
+                if (makes != null)
+                {
+                    List<SelectListItem> makeList = makes.Select(m => new SelectListItem() { Value = m, Text = m }).ToList();
+                    if (makeList != null)
+                        return PartialView("_MakesDropDownList", makeList);
+                    else
+                        throw new Exception("The 'Car' entity result collection is null.", new NoNullAllowedException());
+                }
+                else
+                    throw new Exception("The 'Car' entity result collection is null.", new NoNullAllowedException());
+            }
+            catch(Exception ex)
+            {
+                return View("Error", new HandleErrorInfo(ex, "Cars", "Index"));
+            }
         }
 
         public ActionResult GetModels(string make)
         {
-            var models = db.Cars.Where(c => c.make.Equals(make)).Select(c => c.model_name).Distinct().ToList();
-            List<SelectListItem> modelList = models.Select(m => new SelectListItem() { Value = m, Text = m }).ToList();
-            return PartialView("_ModelsDropDownList", modelList);
+            try
+            {
+                var models = db.Cars.Where(c => c.make.Equals(make)).Select(c => c.model_name).Distinct().ToList();
+                if (models != null)
+                {
+                    List<SelectListItem> modelList = models.Select(m => new SelectListItem() { Value = m, Text = m }).ToList();
+                    if (modelList != null)
+                        return PartialView("_ModelsDropDownList", modelList);
+                    else
+                        throw new Exception("The 'Models' SelectListItems collection is null.", new NoNullAllowedException());
+                }
+                else
+                    throw new Exception("The 'Models' result collection is null.", new NoNullAllowedException());
+            }
+            catch(Exception ex)
+            {
+                return View("Error", new HandleErrorInfo(ex, "Cars", "Index"));
+            }
         }
 
         public ActionResult SearchCars(string term)
         {
-            CarGridViewModel model = new CarGridViewModel()
+            try
             {
-                Cars = PagedListHelpers.GetNextCars(null, null, term, 0, 10),
-                Term = term
-            };
-            return PartialView("_CarsGrid", model);
+                CarGridViewModel model = new CarGridViewModel()
+                {
+                    Cars = PagedListHelpers.GetNextCars(null, null, term, 0, 10),
+                    Term = term
+                };
+                if (model != null)
+                    return PartialView("_CarsGrid", model);
+                else
+                    throw new Exception("The 'CarGridViewModel' object containing the 'Car' entity result collection is null.", new NoNullAllowedException());
+            }
+            catch(Exception ex)
+            {
+                return View("Error", new HandleErrorInfo(ex, "Cars", "Index"));
+            }
         }
 
         // GET: Cars/Details/5
